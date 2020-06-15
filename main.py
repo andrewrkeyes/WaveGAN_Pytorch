fe9e2f309119f58c1cc258513d1f3c520df3fcc1
Andrew Keyes <andykeys@cedar5.cedar.computecanada.ca>
2020-06-15 06:37:17 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,278 @@
+import fire
+import os
+from model import WaveGANGenerator, WaveGANDiscriminator
+import torch
+from torch import nn
+from torch import autograd
+import numpy as np
+from utils import AudioDirectoryDataset, get_data_loader, natural_keys, ToTensor, Normalize
+import torch.distributions as dist
+from tqdm import tqdm
+from torch.autograd import Variable
+from torch.utils.tensorboard import SummaryWriter
+
+print("Done imports")
+
+
+class Main(object):
+    def __init__(self, models_folder='./models', model='main', latent_dim=128, data_folder='./data/nsynth-train/audio', csv_file='files_processed.txt',
+                 valid_data_folder='./data/nsynth-valid/audio', valid_csv_file='files_processed.txt',
+                 cuda=torch.cuda.is_available(), fps=16384,
+                 logs_folder='./logs', n_epochs=5000,
+                 batch_size=64, n_cpu=1, sample_interval=50, lambda_gp=10):
+        super(Main, self).__init__()
+        assert os.path.exists(data_folder), f"{data_folder} does not exists."
+        self.n_cpu = n_cpu
+        self.lambda_gp = lambda_gp
+        self.sample_interval = sample_interval
+        self.fps = fps
+        self.Tensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
+        self.batch_size = batch_size * 1# torch.cuda.device_count()
+        self.n_epochs = n_epochs
+        self.data_folder = data_folder
+        self.csv_file = csv_file
+        self.valid_data_folder = valid_data_folder
+        self.valid_csv_file = valid_csv_file
+        self.models_folder = os.path.join(models_folder, model)
+        self.logs_folder = os.path.join(logs_folder, model)
+        os.makedirs(self.models_folder, exist_ok=True)
+        os.makedirs(self.logs_folder, exist_ok=True)
+        self.cuda = cuda
+        self.model = model
+        self.latent_dim = latent_dim
+        print("creating models")
+        self.generator = WaveGANGenerator()
+        self.discriminator = WaveGANDiscriminator()
+        print("models created")
+        if cuda:
+            # if torch.cuda.device_count() > 1:
+            self.generator = nn.DataParallel(self.generator)
+            self.discriminator = nn.DataParallel(self.discriminator)
+            self.generator.cuda()
+            self.discriminator.cuda()
+        self.distribution = dist.normal.Normal(0, 1)
+        self.writer = SummaryWriter(log_dir=self.logs_folder)
+        print(f"tensorboard --logdir={self.logs_folder}")
+
+    def load_model(self, train=False):
+        if len(os.listdir(self.models_folder)) > 0:
+            files = [f for f in os.listdir(self.models_folder) if os.path.isfile(os.path.join(self.models_folder, f))]
+            files.sort(key=natural_keys)
+            print(f'Loading Model {files[-1]}....')
+            checkpoint = torch.load(os.path.join(self.models_folder, files[-1]),
+                                    map_location='cuda' if self.cuda else 'cpu')
+            self.discriminator.load_state_dict(checkpoint['discriminator_state_dict'], strict=False)
+            self.generator.load_state_dict(checkpoint['generator_state_dict'], strict=False)
+            if train:
+                self.discriminator.train()
+                self.generator.train()
+            else:
+                self.discriminator.eval()
+                self.generator.eval()
+            return checkpoint['epoch']
+        return 0
+
+    def compute_gradient_penalty(self, D, real_samples, fake_samples):
+        """Calculates the gradient penalty loss for WGAN GP"""
+        # Random weight term for interpolation between real and fake samples
+        alpha = self.Tensor(np.random.random((real_samples.size(0), 1, 1)))
+        # Get random interpolation between real and fake samples
+        interpolates = (alpha * real_samples + ((1 - alpha) * fake_samples)).requires_grad_(True)
+        d_interpolates = D(interpolates)
+        fake = Variable(self.Tensor(real_samples.shape[0], 1).fill_(1.0), requires_grad=False)
+        # Get gradient w.r.t. interpolates
+        gradients = torch.autograd.grad(
+            outputs=d_interpolates,
+            inputs=interpolates,
+            grad_outputs=fake,
+            create_graph=True,
+            retain_graph=True,
+            only_inputs=True,
+        )[0]
+        gradients = gradients.view(gradients.size(0), -1)
+        gradient_penalty = ((gradients.norm(2, dim=1) - 1) ** 2).mean()
+        return gradient_penalty
+
+    # Adapted from https://github.com/caogang/wgan-gp/blob/master/gan_toy.py
+    def calc_gradient_penalty(self, net_dis, real_data, fake_data, batch_size, lmbda, use_cuda=False):
+        # Compute interpolation factors
+        alpha = torch.rand(batch_size, 1, 1)
+        alpha = alpha.expand(real_data.size())
+        alpha = alpha.cuda() if use_cuda else alpha
+
+        # Interpolate between real and fake data.
+        interpolates = alpha * real_data + (1 - alpha) * fake_data
+        if use_cuda:
+            interpolates = interpolates.cuda()
+        interpolates = autograd.Variable(interpolates, requires_grad=True)
+
+        # Evaluate discriminator
+        disc_interpolates = net_dis(interpolates)
+
+        # Obtain gradients of the discriminator with respect to the inputs
+        gradients = autograd.grad(outputs=disc_interpolates, inputs=interpolates,
+                                grad_outputs=torch.ones(disc_interpolates.size()).cuda() if use_cuda else
+                                torch.ones(disc_interpolates.size()),
+                                create_graph=True, retain_graph=True, only_inputs=True)[0]
+        gradients = gradients.view(gradients.size(0), -1)
+
+        # Compute MSE between 1.0 and the gradient of the norm penalty to make discriminator
+        # to be a 1-Lipschitz function.
+        gradient_penalty = lmbda * ((gradients.norm(2, dim=1) - 1) ** 2).mean()
+        return gradient_penalty
+
+    def train(self, g_lr=1e-4, d_lr=1e-4, betas=(0.5, 0.9), n_critic=5, model_save=1):
+        optimizer_G = torch.optim.Adam(self.generator.parameters(), g_lr,
+                                       betas)
+        optimizer_D = torch.optim.Adam(self.discriminator.parameters(), d_lr,
+                                       betas)
+        print('Optimizers Ready....')
+        dataset = AudioDirectoryDataset(self.data_folder, self.csv_file, fps=self.fps)
+        valid_dataset = AudioDirectoryDataset(self.valid_data_folder, self.valid_csv_file, fps=self.fps)
+        print('Dataset Ready....')
+        dataloader = get_data_loader(dataset, batch_size=self.batch_size, num_workers=self.n_cpu)
+        valid_dataloader = get_data_loader(valid_dataset, batch_size=self.batch_size, num_workers=self.n_cpu)
+        print('Dataloader Ready....')
+        offset = self.load_model(train=True)
+        print('Training is starting....')
+        loss_log = tqdm(total=0, position=0, bar_format='{desc}')
+        for epoch in range(self.n_epochs):
+            for i, (audio, target) in enumerate(dataloader):
+                if(i > 3490):
+                    break
+                for p in self.discriminator.parameters():
+                    p.requires_grad = True
+
+                self.discriminator.zero_grad()
+                one = torch.tensor(1, dtype=torch.float)
+                neg_one = one * -1
+                if self.cuda:
+                    one = one.cuda()
+                    neg_one = neg_one.cuda()
+                    audio = audio.cuda()
+
+                #############################
+                # (1) Train Discriminator
+                #############################
+                noise = torch.Tensor(self.batch_size, self.latent_dim).uniform_(-1, 1)
+                if self.cuda:
+                    noise = noise.cuda()
+
+                noise_Var = Variable(noise, requires_grad=False)
+                real_data = Variable(audio.type(self.Tensor), requires_grad=False)
+
+                # a) compute loss contribution from real training data
+                D_real = self.discriminator(real_data)
+                D_real = D_real.mean()
+                D_real.backward(neg_one)
+
+                # b) compute loss contribution from generated data, then backprop.
+                fake = Variable(self.generator(noise_Var).data)
+                D_fake = self.discriminator(fake)
+                D_fake = D_fake.mean()
+                D_fake.backward(one)
+
+                # c) compute gradient penalty and backprop
+                gradient_penalty = self.calc_gradient_penalty(self.discriminator, real_data.data,
+                                                     fake.data, self.batch_size, self.lambda_gp,
+                                                     use_cuda=self.cuda)
+                gradient_penalty.backward(one)
+
+                # Compute cost * Wassertein loss..
+                D_cost_train = D_fake - D_real + gradient_penalty
+                D_wass_train = D_real - D_fake
+
+                # Update gradient of discriminator.
+                optimizer_D.step()
+
+                # Train the generator every n_critic steps
+                if i % n_critic == 0:
+                    for p in self.discriminator.parameters():
+                        p.requires_grad = False
+                    self.generator.zero_grad()
+                    noise = torch.Tensor(self.batch_size, self.latent_dim).uniform_(-1, 1)
+                    if self.cuda:
+                        noise = noise.cuda()
+                    noise_Var = Variable(noise, requires_grad=False)
+                    fake = self.generator(noise_Var)
+                    G = self.discriminator(fake)
+                    G = G.mean()
+                    # Update gradients.
+                    G.backward(neg_one)
+                    G_cost = -G
+                    optimizer_G.step()
+
+                loss_log.set_description_str(
+                    '[Epoch %d/%d] [Batch %d/%d] [G loss: %f] [D loss: %f]' % (
+                        epoch + offset, self.n_epochs, i, len(dataloader), G_cost.item(), D_cost_train.item())
+                )
+                n_iter = (epoch + offset) * len(dataloader) + i
+                self.writer.add_scalar('Loss/g_loss', G_cost.item(), n_iter)
+                self.writer.add_scalar('Loss/d_loss', D_cost_train.item(), n_iter)
+
+            #############################
+            # (2) Compute Valid data
+            #############################
+            D_cost = []
+            D_wass = []
+            for i, (audio, target) in enumerate(valid_dataloader):
+                if(i > 150):
+                    break
+                self.discriminator.zero_grad()
+                if(self.cuda):
+                    audio = audio.cuda()
+
+                valid_data_Var = Variable(audio, requires_grad=False)
+                D_real_valid = self.discriminator(valid_data_Var)
+                D_real_valid = D_real_valid.mean()  # avg loss
+
+                # b) compute loss contribution from generated data, then backprop.
+                fake_valid = self.generator(noise_Var)
+                D_fake_valid = self.discriminator(fake_valid)
+                D_fake_valid = D_fake_valid.mean()
+
+                # c) compute gradient penalty and backprop
+                gradient_penalty_valid = self.calc_gradient_penalty(self.discriminator, valid_data_Var.data,
+                                                            fake_valid.data, self.batch_size, self.lambda_gp,
+                                                            use_cuda=self.cuda)
+                # Compute metrics and record in batch history.
+                D_cost_valid = (D_fake_valid - D_real_valid + gradient_penalty_valid)
+                D_wass_valid = D_real_valid - D_fake_valid
+
+                if self.cuda:
+                    D_cost_valid = D_cost_valid.cpu()
+                    D_wass_valid = D_wass_valid.cpu()
+
+                D_cost.append(D_cost_valid.data.numpy())
+                D_wass.append(D_wass_valid.data.numpy())
+
+            self.writer.add_scalar('Validation/d_cost', sum(D_cost) / float(len(D_cost)), epoch)
+            self.writer.add_scalar('Validation/d_wass', sum(D_wass) / float(len(D_wass)), epoch)
+
+
+            if (epoch + offset + 1) % model_save == 0:
+                self.save_model(epoch + offset + 1)
+
+    def save_model(self, epoch):
+        os.makedirs(self.models_folder, exist_ok=True)
+        torch.save({
+            'epoch': epoch,
+            'discriminator_state_dict': self.discriminator.state_dict(),
+            'generator_state_dict': self.generator.state_dict(),
+        }, os.path.join(self.models_folder, f"{epoch}"))
+
+    def generate_samples(self):
+        offset = self.load_model(train=False)
+        z = self.distribution.sample([self.batch_size, self.latent_dim])
+        if self.cuda:
+            z = z.cuda()
+        self.writer.add_histogram('gen_distribution', z)
+        z = self.generator(z)
+        for i in range(z.shape[0]):
+            np.save("samples/epoch_"+str(offset)+"_sample_"+str(i), z.cpu().detach().numpy()[i].reshape((-1, )))
+            self.writer.add_audio(f'Samples {i}', z.cpu().detach().numpy()[i].reshape((-1, )), offset,
+                                  sample_rate=self.fps)
+
+
+if __name__ == '__main__':
+    fire.Fire(Main)
---
+++ utils.py
@@ -0,0 +1,341 @@
+import os
+import numpy as np
+from torch.utils.data import Dataset
+from pathlib import Path
+import torch
+import re
+# import torchaudio as ta
+import pickle
+import lmdb
+import os.path as osp
+from scipy.signal import resample
+# from moviepy.editor import VideoFileClip
+from torchvision import transforms, io
+import librosa as ta
+from librosa import core as ap
+from functools import partial
+import librosa
+import pandas as pd
+
+def atoi(text):
+    return int(text) if text.isdigit() else text
+
+
+def natural_keys(text):
+    '''
+    alist.sort(key=natural_keys) sorts in human order
+    http://nedbatchelder.com/blog/200712/human_sorting.html
+    (See Toothy's implementation in the comments)
+    '''
+    return [atoi(c) for c in re.split(r'(\d+)', text)]
+
+
+def get_data_loader(dataset, batch_size, num_workers):
+    """
+    generate the data_loader from the given dataset
+    :param dataset: F2T dataset
+    :param batch_size: batch size of the data
+    :param num_workers: num of parallel readers
+    :return: dl => dataloader for the dataset
+    """
+    from torch.utils.data import DataLoader
+
+    dl = DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=num_workers,
+        pin_memory=False
+    )
+
+    return dl
+
+
+class AudioDirectoryDataset(Dataset):
+    """ pyTorch Dataset wrapper for the generic flat directory dataset """
+
+    def __setup_files(self):
+        """
+        private helper for setting up the files_list
+        :return: files => list of paths of files
+        """
+        file_names = Path(self.data_dir).glob('**/*.wav')
+        files = []  # initialize to empty list
+
+        for file_name in file_names:
+            possible_file = str(file_name)
+            if os.path.isfile(possible_file) and '.wav' in possible_file:
+                files.append(possible_file)
+
+        # return the files list
+        return files
+
+    def __init__(self, data_dir, csv_file, transform=None, fps=16000):
+        """
+        constructor for the class
+        :param data_dir: path to the directory containing the data
+        :param transform: transforms to be applied to the images
+        """
+        # define the state of the object
+        self.data_dir = data_dir
+        self.transform = transform
+        self.fps = fps
+        self.resample = partial(librosa.core.resample, 16000, fps)
+        csv_file = os.path.join(data_dir, csv_file)
+        self.mem_frame = pd.read_csv(csv_file)
+
+    def __len__(self):
+        """
+        compute the length of the dataset
+        :return: len => length of dataset
+        """
+        return len(self.mem_frame)
+
+    def __getitem__(self, idx):
+        """
+        obtain the image (read and transform)
+        :param idx: index of the file required
+        :return: img => image array
+        """
+        # read the image:
+        #audio = self.files[idx]
+        audio = os.path.join(self.data_dir, self.mem_frame.iloc[idx, 0])
+        audio, sr = librosa.core.load(audio)
+        audio = librosa.core.resample(audio, sr, self.fps)
+        audio = np.reshape(audio, (1,-1))
+        if audio.shape[0] > 1:
+            audio = np.mean(audio, axis=1)
+        if audio.shape[-1] < self.fps:
+            n_samples = self.fps
+            total_samples = audio.shape[-1]
+            n_pad = n_samples - total_samples
+            audio = torch.nn.functional.pad(audio.view(1, 1, -1), (0, n_pad), mode='replicate').view(1, -1)
+        elif audio.shape[-1] > self.fps:
+            audio = audio[:, 0:self.fps]
+        return audio, 1
+
+
+class Normalize:
+    """Applies the :class:`~torchvision.transforms.Normalize` transform to a batch of images.
+    .. note::
+        This transform acts out of place by default, i.e., it does not mutate the input tensor.
+    Args:
+        mean (sequence): Sequence of means for each channel.
+        std (sequence): Sequence of standard deviations for each channel.
+        inplace(bool,optional): Bool to make this operation in-place.
+        dtype (torch.dtype,optional): The data type of tensors to which the transform will be applied.
+        device (torch.device,optional): The device of tensors to which the transform will be applied.
+    """
+
+    def __init__(self, mean, std, inplace=False, dtype=torch.float, device='cpu'):
+        self.mean = torch.as_tensor(mean, dtype=dtype, device=device)[None, :, None, None]
+        self.std = torch.as_tensor(std, dtype=dtype, device=device)[None, :, None, None]
+        self.inplace = inplace
+
+    def __call__(self, tensor):
+        """
+        Args:
+            tensor (Tensor): Tensor of size (N, C, H, W) to be normalized.
+        Returns:
+            Tensor: Normalized Tensor.
+        """
+        if not self.inplace:
+            tensor = tensor.clone()
+
+        tensor.sub_(self.mean).div_(self.std)
+        return tensor
+
+
+class ToTensor:
+    """Applies the :class:`~torchvision.transforms.ToTensor` transform to a batch of images.
+    """
+
+    def __init__(self):
+        self.max = 255
+
+    def __call__(self, tensor):
+        """
+        Args:
+            tensor (Tensor): Tensor of size (N, C, H, W) to be tensorized.
+        Returns:
+            Tensor: Tensorized Tensor.
+        """
+        return tensor.float().div_(self.max)
+
+
+class VideoDirectoryDataset(Dataset):
+    """ pyTorch Dataset wrapper for the generic flat directory dataset """
+
+    def __init__(self, data_dir, audio_fps=16384, image_size=224, video_fps=16):
+        """
+        constructor for the class
+        :param data_dir: path to the directory containing the data
+        :param transform: transforms to be applied to the images
+        """
+        # define the state of the object
+        self.data_dir = data_dir
+        self.transform = transforms.Compose([Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]), ])
+        self.audio_fps = audio_fps
+        self.image_size = image_size
+        self.video_fps = video_fps
+
+        self.classes = os.listdir(data_dir)
+        self.classes.sort()
+
+        file_names = Path(self.data_dir).glob('**/*.mp4')
+        self.files = []  # initialize to empty list
+        for file_name in file_names:
+            self.files.append(str(file_name))
+        print(f"We have {len(self.files)} videos in total!")
+
+    def __len__(self):
+        """
+        compute the length of the dataset
+        :return: len => length of dataset
+        """
+        return len(self.files)
+
+    def get_class(self, x):
+        for i in self.classes:
+            if i in x:
+                return self.classes.index(i)
+
+    def preprocess_video(self, x):
+        if x.shape[0] > self.video_fps:
+            x = x[np.sort(np.random.choice(list(range(x.shape[0])), self.video_fps, False))]
+        elif x.shape[0] < self.video_fps:
+            x = x[np.sort(np.random.choice(list(range(x.shape[0])), self.video_fps))]
+        x = torch.nn.functional.interpolate(x.float().permute(0, 3, 1, 2),
+                                            (self.image_size, self.image_size), mode='bilinear', align_corners=True)
+        if self.transform:
+            x = self.transform(x)
+        return x
+
+    def preprocess_audio(self, x, audio_fps):
+        x = ap.to_mono(x.numpy())
+        if audio_fps != self.audio_fps:
+            x = ap.resample(x, audio_fps, self.audio_fps)
+        if x.shape[0] < self.audio_fps:
+            x = np.pad(x, (0, self.audio_fps - x.shape[0]))
+        return x.reshape((1, -1))
+
+    def __getitem__(self, idx):
+        """
+        obtain the image (read and transform)
+        :param idx: index of the file required
+        :return: img => image array
+        """
+        rand = np.random.RandomState()
+        video = self.files[idx]
+        target = self.get_class(video)
+        video, audio, info = io.read_video(video)
+
+        video_duration = video.shape[0] // info['video_fps']
+
+        # Less than one second video
+        if video_duration == 0:
+            return self.preprocess_video(video), self.preprocess_audio(audio, info['audio_fps']), target
+        else:
+            second_idx = rand.randint(0, video_duration - 1)
+            second_idx_vid = int(second_idx * info['video_fps'])
+            second_idx_aud = int(second_idx * info['audio_fps'])
+
+            video = video[second_idx_vid:second_idx_vid + int(info['video_fps'])]
+            audio = audio[:, second_idx_aud:second_idx_aud + int(info['audio_fps'])]
+
+            return self.preprocess_video(video), self.preprocess_audio(audio, info['audio_fps']), target
+
+
+class VideoDirectoryDataset2(Dataset):
+    """ pyTorch Dataset wrapper for the generic flat directory dataset """
+
+    def __init__(self, data_dir, audio_fps=16384, image_size=224, video_fps=16):
+        """
+        constructor for the class
+        :param data_dir: path to the directory containing the data
+        :param transform: transforms to be applied to the images
+        """
+        # define the state of the object
+        self.data_dir = data_dir
+        self.transform = transforms.Compose([ToTensor(),
+                                             Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]), ])
+        self.audio_fps = audio_fps
+        self.image_size = image_size
+        self.video_fps = video_fps
+
+        self.classes = os.listdir(data_dir)
+        self.classes.sort()
+
+        file_names = Path(self.data_dir).glob('**/*.mp4')
+        self.files = []  # initialize to empty list
+        for file_name in file_names:
+            self.files.append(str(file_name))
+        print(f"We have {len(self.files)} videos in total!")
+
+    def __len__(self):
+        """
+        compute the length of the dataset
+        :return: len => length of dataset
+        """
+        return len(self.files)
+
+    def get_class(self, x):
+        for i in self.classes:
+            if i in x:
+                return self.classes.index(i)
+
+    def __getitem__(self, idx):
+        """
+        obtain the image (read and transform)
+        :param idx: index of the file required
+        :return: img => image array
+        """
+        rand = np.random.RandomState()
+
+        try:
+            video = self.files[idx]
+            target = self.get_class(video)
+            video = VideoFileClip(video, audio_fps=self.audio_fps, target_resolution=(self.image_size, self.image_size))
+            video_fps = int(video.fps // 1)
+            video_duration = video.duration // 1
+            audio = video.audio.to_soundarray()
+            video = np.array(list(video.iter_frames()))
+
+            second_idx = rand.randint(0, video_duration - 1)
+            second_idx_vid = second_idx * video_fps
+            second_idx_aud = second_idx * self.audio_fps
+
+            video = video[second_idx_vid:second_idx_vid + video_fps]
+            video = resample(video, self.video_fps, axis=0)
+            if self.transform:
+                video = self.transform(torch.tensor(video).permute(0, 3, 1, 2))
+            audio = audio[second_idx_aud:second_idx_aud + self.audio_fps]
+            if audio.shape[1] > 1:
+                audio = audio.mean(1).reshape((1, -1))
+            return video, audio, target
+        except:
+            print(self.files[idx])
+            return self.__getitem__(4)
+
+
+class ImageFolderLMDB(Dataset):
+    def __init__(self, db_path):
+        self.db_path = db_path
+        self.env = lmdb.open(db_path, subdir=osp.isdir(db_path),
+                             readonly=True, lock=False,
+                             readahead=False, meminit=False)
+        with self.env.begin(write=False) as txn:
+            self.length = pickle.loads(txn.get(b'__len__'))
+            self.keys = pickle.loads(txn.get(b'__keys__'))
+
+    def __getitem__(self, index):
+        env = self.env
+        with env.begin(write=False) as txn:
+            byteflow = txn.get(self.keys[index])
+        return pickle.loads(byteflow)
+
+    def __len__(self):
+        return self.length
+
+    def __repr__(self):
+        return self.__class__.__name__ + ' (' + self.db_path + ')'
